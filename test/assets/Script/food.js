ddfab05b3544e4a34a8dabbe2c8138298d0f4180
Adjlist <997285915@qq.com>
2017-10-03 16:44:54 +0800

    h5

---
+++ test/assets/Script/food.js
@@ -0,0 +1,33 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+    onPicked: function() {
+        // 当星星被收集时，调用 Game 脚本中的接口，生成一个新的星星
+        this.game.spawnNewStar();
+        // 调用 Game 脚本的得分方法
+        this.game.gainScore();
+        // 然后销毁当前星星节点
+        this.node.destroy();
+    },
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
